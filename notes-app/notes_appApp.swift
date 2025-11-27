47d35e8b3cb70a5d56ea1e61113a245d4e85f91b
dafariski77 <dafariski555@gmail.com>
2025-11-27 20:17:26 +0700

    Initial Commit

---
+++ notes-app/notes_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  notes_appApp.swift
+//  notes-app
+//
+//  Created by Dafa Riski on 27/11/25.
+//
+
+import SwiftUI
+
+@main
+struct notes_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
