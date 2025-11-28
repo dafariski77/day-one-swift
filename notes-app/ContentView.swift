97a686890e08d70344ae5555abbbfff8b2e4e441
dafariski77 <dafariski555@gmail.com>
2025-11-28 21:23:44 +0700

    day-2: component, props, conditional

--- notes-app/ContentView.swift
+++ notes-app/ContentView.swift
@@ -9,17 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding(10)
         HStack {
-            Text("Hello world")
-            Text("Hello world")
+            DayForecast(day: "Senin", high: 12, low: 12, isRainy: false)
+            DayForecast(day: "Selasa", high: 20, low: 8, isRainy: true)
         }
+    }
+}
+
+struct DayForecast: View {
+    let day: String
+    let high: Int
+    let low: Int
+    let isRainy: Bool
+    
+    var iconName: String {
+        if isRainy {
+            return "cloud.rain.fill"
+        } else {
+            return "sun.max.fill"
+        }
+    }
+       
+    var iconColor: Color {
+        if isRainy {
+            return Color.blue
+        } else {
+            return Color.yellow
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            Text(day).font(Font.headline)
+            Image(systemName: iconName)
+                .foregroundStyle(iconColor)
+                .font(Font.largeTitle)
+                .padding(5)
+            Text("High: \(high)")
+                .fontWeight(Font.Weight.semibold)
+            Text("Low: \(low)")
+                .fontWeight(Font.Weight.medium)
+        }
+        .padding()
     }
 }
 
